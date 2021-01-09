0821528094622cc45067c6d55df1b252b13ea446
Omkar Shivadekar <omkarshivadekar.os@gmail.com>
2021-01-09 22:02:48 +0530

    #1 - added configuration

--- src/main/java/com/reddit/clone/config/SecurityConfig.java
+++ src/main/java/com/reddit/clone/config/SecurityConfig.java
@@ -1,9 +1,28 @@
 package com.reddit.clone.config;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/api/auth/**").permitAll()
+			.antMatchers("/h2/**").permitAll()
+			.anyRequest()
+			.authenticated()
+			.and().headers().frameOptions().sameOrigin();
+			
+		
+			
+	}
+	
+	
 
 }
